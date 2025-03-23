97e8f1eff81f5713b04237cd98500e46e51c8b6a
lehuysang <lehuysang01@gmail.com>
2025-03-23 14:15:08 +0700

    initial commit

---
+++ Mobiles_dataset/Mobiles_dataset.py
@@ -0,0 +1,7 @@
+#https://www.kaggle.com/datasets/abdulmalik1518/mobiles-dataset-2025
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('/home/sang/BaiTap/MachineLearning/Mobiles_dataset/Mobiles Dataset (2025).csv', encoding='latin1')
